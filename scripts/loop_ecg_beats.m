a96e927f40bfc13d8f1e4555489e190aeb0c43c5
Davi Nascimento <davi.nascimento777@gmail.com>
2022-07-22 21:45:50 -0300

    extraindo batimentos do ecg

--- scripts/loop_ecg_beats.m
+++ scripts/loop_ecg_beats.m
@@ -1,52 +1,60 @@
-
+clc;
 clear all;
 
-d = dotenv('../.env');
-
-
+d = dotenv('./.env');
 
 %% Carregando dados
-f = d.env.ROOT_PATH + '/resources/reports/train.csv';
-f_details = d.env.ROOT_PATH + '/resources/reports/train_details.csv';
+f = d.env.ROOT_PATH + '/resources/reports/ecg_files_dataset_dev.csv';
+path = d.env.DATALAKE_PATH;
+
+df = readtable(f);
 
 
-t = readtable(f);
-t_details = readtable(f_details);
+%% Loop de processamento
+for i=1:height(df)
+    try
+        % definindo path
+        b = table2cell(df(i,'file'));
+        a = cellstr(b);
+        c = char(a);
+        d = split(strrep(c,'tse','parquet'),'/');
 
-% fid = fopen(fname); 
-% raw = fread(fid,inf); 
-% str = char(raw'); 
-% fclose(fid); 
-% val = jsondecode(str);
-% 
-% labels = {'ictal', 'normal', 'pos-ictal', 'pre-ictal', 'recuperacao'};
-% 
-% for i = 1:length(val)
-%     itens = val(i).collections;
-%     
-%     for j = 1:length(itens)
-%         name = strrep(itens(j).name, 'edf', 'parquet');
-%         
-%         for k = 1:length(labels)
-%             try
-%                 file = d.env.DATALAKE_PATH + '/siena/raw/periods/ecg/' + labels{k} + '/' + name;
-%                 t = parquetread(file);
-%                 x = table2array(t);
-%                 disp(file);
-%                 
-%                 [B,P,QRS,T] = ECGsegmentationF(x, fs);
-%                 
-%                 s = split(file, '/');
-%                 
-%                 name_mat = strrep(s{end}, 'parquet','mat');
-% 
-%                 path_file = d.env.DATALAKE_PATH + '/siena/processed/ecg_beats/' + labels{k} + '/' + name_mat;
-%                 disp(path_file);
-%                 save(path_file,'B','P','QRS','T');
-% 
-%             catch ME
-%                 continue 
-%             end
-%         end
-%     end
-% end
+        file_path = join([path, "tuh_seizures/raw/ecg/dev",d{end}],"/");
+    
+        % trabalhando freq
+        bb = table2cell(df(i,'freq'));
+        fs = bb{1};
+
+        % intervalos
+        inicio = table2cell(df(i,'Start'));
+        inicio = inicio{1};
+
+        fim = table2cell(df(i,'Stop'));
+        fim = fim{1};
+
+        % carregando parquet
+        s = parquetread(file_path);
+        x = table2array(s);
+        x = x(inicio*fs:fim*fs);
+        
+        % tipo de crise
+        seizure_type = table2cell(df(i,'SeizureType'));
+        seizure_type = seizure_type{1};
+
+        % Capturando batimentos
+        [B,P,QRS,T] = ECGsegmentationF(x, fs);
+        
+        name_mat = strrep(d{end}, 'parquet','mat');
+        
+        path_file = path + '/tuh_seizures/processed/ecg_beats/' + name_mat;
+        
+        disp(path_file);
+
+        save(path_file,'B','P','QRS','T', 'seizure_type');
+    
+    catch ME
+        continue 
+    end
+end
+
+
